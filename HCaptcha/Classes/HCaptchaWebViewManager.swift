3e1e23cb915dd61ca755b8a09ac5b18f0455445f
Mikhail Rubanov <m.rubanov@dodopizza.com>
2021-08-13 11:13:06 +0500

    Add size modes

--- HCaptcha/Classes/HCaptchaWebViewManager.swift
+++ HCaptcha/Classes/HCaptchaWebViewManager.swift
@@ -100,13 +100,16 @@ internal class HCaptchaWebViewManager {
          - baseURL: The URL configured with the sitekey
          - endpoint: The JS API endpoint to be loaded onto the HTML file.
      */
-    init(html: String, apiKey: String, baseURL: URL, endpoint: String) {
+    init(html: String, apiKey: String, baseURL: URL, endpoint: String, size: Size) {
         self.endpoint = endpoint
         self.decoder = HCaptchaDecoder { [weak self] result in
             self?.handle(result: result)
         }
 
-        let formattedHTML = String(format: html, arguments: ["apiKey": apiKey, "endpoint": endpoint])
+        let formattedHTML = String(format: html,
+                                   arguments: ["apiKey": apiKey,
+                                               "endpoint": endpoint,
+                                               "size": size.rawValue])
 
         if let window = UIApplication.shared.keyWindow {
             setupWebview(on: window, html: formattedHTML, url: baseURL)
