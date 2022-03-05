62c7826e69aef8b85b0a634df08229aa882a288b
Alex Bobrikovich <abobrikovich@gmail.com>
2022-03-05 13:09:37 +0000

    #49 Fix 'nil while unwrapping an Optional value' on URL construction (#51)

--- HCaptcha/Classes/HCaptchaDebugInfo.swift
+++ HCaptcha/Classes/HCaptchaDebugInfo.swift
@@ -16,15 +16,35 @@ extension String {
     }
 }
 
+private func updateInfoFor(_ image: String, _ ctx: UnsafeMutablePointer<CC_MD5_CTX>) {
+    var count: UInt32 = 0
+    if let imagePtr = (image as NSString).utf8String {
+        let classes = objc_copyClassNamesForImage(imagePtr, &count)
+        for cls in UnsafeBufferPointer<UnsafePointer<CChar>>(start: classes, count: Int(count)) {
+            CC_MD5_Update(ctx, cls, CC_LONG(strlen(cls)))
+        }
+        classes?.deallocate()
+    }
+}
+
+private func getFinalHash(_ ctx: UnsafeMutablePointer<CC_MD5_CTX>) -> String {
+    var digest: [UInt8] = Array(repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+    CC_MD5_Final(&digest, ctx)
+    let hexDigest = digest.map { String(format: "%02hhx", $0) }.joined()
+    return hexDigest
+}
+
+private func bundleShortVersion() -> String {
+    let sdkBundle = Bundle(for: HCaptchaDebugInfo.self)
+    let sdkBundleShortVer = sdkBundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    return sdkBundleShortVer?.jsSanitize() ?? "unknown"
+}
+
 class HCaptchaDebugInfo {
 
-    private(set) public var json: String!
+    public let json: String = HCaptchaDebugInfo.buildDebugInfoJson()
 
-    init() {
-        self.json = self.buildDebugInfoJson()
-    }
-
-    private func buildDebugInfoJson() -> String {
+    private class func buildDebugInfoJson() -> String {
         let failsafeJson = "[]"
         let encoder = JSONEncoder()
         guard let jsonData = try? encoder.encode(buildDebugInfo()) else { return failsafeJson }
@@ -32,7 +52,7 @@ class HCaptchaDebugInfo {
         return json
     }
 
-    private func buildDebugInfo() -> [String] {
+    private class func buildDebugInfo() -> [String] {
         let depsCtx = UnsafeMutablePointer<CC_MD5_CTX>.allocate(capacity: 1)
         let sysCtx = UnsafeMutablePointer<CC_MD5_CTX>.allocate(capacity: 1)
         let appCtx = UnsafeMutablePointer<CC_MD5_CTX>.allocate(capacity: 1)
@@ -41,18 +61,18 @@ class HCaptchaDebugInfo {
         CC_MD5_Init(appCtx)
 
         for framework in Bundle.allFrameworks {
-            let frameworkPath = URL(string: framework.bundlePath)!
+            guard let frameworkPath = URL(string: framework.bundlePath) else { continue }
             let frameworkBin = frameworkPath.deletingPathExtension().lastPathComponent
             let image = frameworkPath.appendingPathComponent(frameworkBin).absoluteString
             let systemFramework = image.contains("/Library/PrivateFrameworks/") ||
                                   image.contains("/System/Library/Frameworks/")
 
             let md5Ctx = systemFramework ? sysCtx : depsCtx
-            self.updateInfoFor(image, md5Ctx)
+            updateInfoFor(image, md5Ctx)
         }
 
         if let executablePath = Bundle.main.executablePath {
-            self.updateInfoFor(executablePath, appCtx)
+            updateInfoFor(executablePath, appCtx)
         }
 
         let depsHash = getFinalHash(depsCtx)
@@ -67,29 +87,5 @@ class HCaptchaDebugInfo {
             "iver_\(String(describing: iver))",
             "sdk_\(bundleShortVersion())"
         ]
-    }
-
-    private func updateInfoFor(_ image: String, _ ctx: UnsafeMutablePointer<CC_MD5_CTX>) {
-        var count: UInt32 = 0
-        if let imagePtr = (image as NSString).utf8String {
-            let classes = objc_copyClassNamesForImage(imagePtr, &count)
-            for cls in UnsafeBufferPointer<UnsafePointer<CChar>>(start: classes, count: Int(count)) {
-                CC_MD5_Update(ctx, cls, CC_LONG(strlen(cls)))
-            }
-            classes?.deallocate()
-        }
-    }
-
-    private func getFinalHash(_ ctx: UnsafeMutablePointer<CC_MD5_CTX>) -> String {
-        var digest: [UInt8] = Array(repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-        CC_MD5_Final(&digest, ctx)
-        let hexDigest = digest.map { String(format: "%02hhx", $0) }.joined()
-        return hexDigest
-    }
-
-    private func bundleShortVersion() -> String {
-        let sdkBundle = Bundle(for: HCaptchaDebugInfo.self)
-        let sdkBundleShortVer = sdkBundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
-        return sdkBundleShortVer?.jsSanitize() ?? "unknown"
     }
 }
