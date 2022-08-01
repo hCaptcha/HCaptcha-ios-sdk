3265973ec33d6d13c563518e06b27c8222e50c08
Alex Bobrikovich <abobrikovich@gmail.com>
2022-08-01 11:23:42 +0200

    #91 add SwiftUI simple example (#94)
    * #91 add SwiftUI simple example
    
    * Remove unused DEVELOPMENT_ASSET_PATHS
    
    * #91 call configureWebView on validate if webView.size is zero
    
    * Fix source code header comments in ObjC Example
    
    * Add link to SwiftUI example
    
    * Fix sonar smells
    
    * Apply suggestions from code review
    
    Co-authored-by: e271828- <e271828-@users.noreply.github.com>

---
+++ Example/SwiftUI/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  HCaptcha_SwiftUI_Example
+//
+//  Copyright © 2022 HCaptcha. MIT License.
+//
+
+import SwiftUI
+
+@main
+struct HCaptchaSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HCaptchaView()
+        }
+    }
+}
